71265eb137fd587438fb93291a23d6f66c187acb
ThomasLesinski <tlesinski@web.de>
2021-02-14 22:10:57 +0100

    Create tm-fix-pr-title.js

---
+++ tm-fix-pr-title.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Ott / GH Shopware - Fix pr title
+// @namespace    https://www.ottscho-it-service.de/
+// @version      1
+// @description  Add btn to fix pr title
+// @author       OttschoThomas
+// @include      https://github.com/*/compare/*?expand=1
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  const prTitleInput = document.querySelector('#pull_request_title');
+  prTitleInput.parentElement.style.flexGrow = '1';
+
+  const btnFixPrTitle = document.createElement('div');
+  btnFixPrTitle.classList.add('btn', 'btn-fix-pr-title');
+  btnFixPrTitle.innerHTML = 'Fix';
+
+  btnFixPrTitle.addEventListener('click', function() {
+    const prTitleText = prTitleInput.value.trim();
+    const prTitleTextSplitted = prTitleText.split(' ');
+
+    if (prTitleTextSplitted[0].toLowerCase() === 'sup' && /^\d+$/.test(prTitleTextSplitted[1])) {
+      const finalPrTitle = `[${prTitleTextSplitted[0].toUpperCase()}-${prTitleTextSplitted[1]}] ${prTitleTextSplitted.slice(2, prTitleTextSplitted.length).join(' ')}`;
+
+      prTitleInput.value = finalPrTitle;
+    }
+  });
+
+  const anchorEl = document.querySelector('.discussion-topic-header');
+  anchorEl.classList.add('d-flex');
+  anchorEl.append(btnFixPrTitle);
+})();
